a090d5e17f29bbbed31c92932a2ad0e0c5f8f1e3
Donal <donalmathewpt@gmail.com>
2025-01-04 11:44:45 +0530

    1st commit

---
+++ src/main.py
@@ -0,0 +1,44 @@
+from clear_sceen import clear_screen
+
+def main():
+    clear_screen()
+
+    flag=True
+    while(flag):
+        print("1: login")
+        print("2: signup")
+        print("0: exit")
+        
+        x=int(input(""))
+        if x==1:
+            login()
+            clear_screen()
+        elif x==2:
+            signup()
+            clear_screen()
+        else:
+            flag=False
+            clear_screen()
+            print("closed successfully...")
+
+def login():
+    clear_screen()
+    
+    flag=True
+    while(flag):
+        print()
+        print("1: log into organisation")
+        print("2: Login as user")
+
+
+def signup():
+    clear_screen()
+    
+    flag=True
+    while(flag):
+        print()
+        print("1: create head organisation")
+        print("2: create a sub organisation")
+        print("3: signup as user")
+
+main()
